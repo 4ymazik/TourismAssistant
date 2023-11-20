b20d245f28c89eb9e41c1ae2a59820750a203f95
4ymazik <alankirpichev760@gmail.com>
2023-11-20 17:28:22 +0300

    Добавлен начальный экран

--- main.py
+++ main.py
@@ -6,14 +6,32 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5.QtCore import Qt
 
 
-class Advisor(QWidget):     # вкладка с советами туристам
+class StarterScreen(QWidget):   # стартовое окно
+    def __init__(self):
+        super().__init__()
+        loadUi("starterscreen.ui", self)
+        self.setWindowTitle("Справочник для туристов")
+        self.btn_advisor.clicked.connect(self.loadadvisorscreen)
+        self.btn_table.clicked.connect(self.loadtablescreen)
+
+    def loadtablescreen(self):
+        self.mainwindow = MainWindow()
+        self.mainwindow.show()
+
+    def loadadvisorscreen(self):
+        self.advisor = Advisor()
+        self.advisor.show()
+
+
+class Advisor(QWidget):  # вкладка с советами туристам
     def __init__(self):
         super().__init__()
         loadUi("advisor.ui", self)
         self.advice_text.setVisible(True)
+        self.setWindowTitle("Основные советы")
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow):  # окно с таблицей
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi("tourismhelp.ui", self)
@@ -27,7 +45,7 @@ class MainWindow(QMainWindow):
 
         self.loaddata()
 
-        self.btn_search.clicked.connect(self.SearchModels)
+        self.btn_search.clicked.connect(self.searchcountries)
         self.btn_info.clicked.connect(self.showadvice)
 
     def loaddata(self):  # Отображение базы данных в таблице
@@ -44,7 +62,7 @@ class MainWindow(QMainWindow):
             self.tableWidget.setItem(tablerow, 3, QtWidgets.QTableWidgetItem(row[3]))
             tablerow += 1
 
-    def SearchModels(self):  # поиск введеного названия страны по таблице
+    def searchcountries(self):  # поиск введеного названия страны по таблице
         name_search = self.search_field.text()
         for row in range(self.tableWidget.rowCount()):
             matching_items = self.tableWidget.findItems(name_search, Qt.MatchContains)
@@ -52,17 +70,17 @@ class MainWindow(QMainWindow):
                 item = self.tableWidget.item(row, 0)
                 self.tableWidget.setRowHidden(row, name_search not in item.text())
 
-    def showadvice(self):   # показать советы
+    def showadvice(self):  # показать советы
         self.advisor = Advisor()
         self.advisor.show()
 
 
 app = QApplication(sys.argv)
-mainwindow = MainWindow()
+starterscreen= StarterScreen()
 widget = QtWidgets.QStackedWidget()
-widget.addWidget(mainwindow)
-widget.setFixedHeight(850)
-widget.setFixedWidth(1120)
+widget.addWidget(starterscreen)
+widget.setFixedHeight(550)
+widget.setFixedWidth(1000)
 widget.show()
 
 sys.exit(app.exec_())
